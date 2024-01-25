c07e8a43fbbafa1942111c4db704c3db23c0e5f7
Tim Thayne <timthayne@gmail.com>
2024-01-25 09:19:51 -0700

    Added Book of Mormon activity

---
+++ week02/scripts/bom.js
@@ -0,0 +1,28 @@
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+button.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  if (input.value) {
+    const li = document.createElement('li');
+    const deleteButton = document.createElement('button');
+
+    li.textContent = input.value;
+    deleteButton.textContent = '❌';
+
+    deleteButton.addEventListener('click', () => {
+      list.removeChild(li);
+      input.focus();
+    });
+
+    li.appendChild(deleteButton);
+
+    list.appendChild(li);
+
+    input.value = '';
+  }
+
+  input.focus();
+});
